97c3dc6eb5ab2b827bc1a320a9b8e4f4590232d5
Timolio <94037690+Timolio@users.noreply.github.com>
2025-02-13 04:54:11 +0100

    Minor changes

--- src/models/SearchSession.js
+++ src/models/SearchSession.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const searchSessionSchema = new mongoose.Schema({
     userId: String,
     results: Array,
-    expiresAt: { type: Date, default: Date.now, expires: 300 },
+    expiresAt: { type: Date, default: Date.now, expires: 3000 },
 });
 
 module.exports = mongoose.model('SearchSession', searchSessionSchema);
